f19192491c2d222d9d76a36ffb39edff2365ad03
SharonJoshi18 <joshisharon01@gmail.com>
2026-03-04 23:38:17 +0530

    Initial setup and fetching Historical Data

---
+++ LearnToCode/main.py
@@ -0,0 +1,54 @@
+from SmartApi import SmartConnect #or from SmartApi.smartConnect import SmartConnect
+import pyotp
+from logzero import logger
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+username = os.getenv("ANGEL_USERNAME")
+pwd = os.getenv("PWD")
+
+smartApi = SmartConnect(api_key)
+
+try:
+    token = os.getenv("TOKEN")
+    totp = pyotp.TOTP(token).now()
+except Exception as e:
+    logger.error("Invalid Token: The provided token is not valid.")
+    raise e
+
+correlation_id = "abcde"
+data = smartApi.generateSession(username, pwd, totp)
+
+
+try:
+    historicParam={
+        "exchange": "BSE",
+        "symboltoken": "24077",
+        "interval": "ONE_MINUTE",
+        "fromdate": "2026-03-04 09:00", 
+        "todate": "2026-03-04 09:16"
+    }
+   
+    print(smartApi.getCandleData(historicParam))
+    response = smartApi.getCandleData(historicParam)
+
+    if response['status']:
+        formatted_data = []
+
+        for candle in response['data']:
+            formatted_data.append({
+                "datetime": candle[0],
+                "open": candle[1],
+                "high": candle[2],
+                "low": candle[3],
+                "close": candle[4],
+                "volume": candle[5]
+            })
+        print(formatted_data)
+    else:
+        print("Error:", response)
+except Exception as e:
+    logger.exception(f"Historic Api failed: {e}")
---
+++ nse_data/list.py
@@ -0,0 +1,68 @@
+stock_data = [
+    {
+        "token": "759814",
+        "symbol": "SMALL250-EQ",
+        "name": "SMALL250",
+        "expiry": "",
+        "strike": "-1.000000",
+        "lotsize": "1",
+        "instrumenttype": "",
+        "exch_seg": "NSE",
+        "tick_size": "1.000000"
+    },
+    {
+        "token": "10576",
+        "symbol": "NIFTYBEES-EQ",
+        "name": "NIFTYBEES",
+        "expiry": "",
+        "strike": "-1.000000",
+        "lotsize": "1",
+        "instrumenttype": "",
+        "exch_seg": "NSE",
+        "tick_size": "1.000000"
+    },
+    {
+        "token": "24077",
+        "symbol": "MID150CASE-EQ",
+        "name": "MID150CASE",
+        "expiry": "",
+        "strike": "-1.000000",
+        "lotsize": "1",
+        "instrumenttype": "",
+        "exch_seg": "NSE",
+        "tick_size": "1.000000"
+    },
+    {
+        "token": "544193",
+        "symbol": "MID150CASE",
+        "name": "MID150CASE",
+        "expiry": "",
+        "strike": "-1.000000",
+        "lotsize": "1",
+        "instrumenttype": "",
+        "exch_seg": "BSE",
+        "tick_size": "1.000000"
+    },
+    {
+        "token": "544605",
+        "symbol": "SMALL250",
+        "name": "SMALL250",
+        "expiry": "",
+        "strike": "-1.000000",
+        "lotsize": "1",
+        "instrumenttype": "",
+        "exch_seg": "BSE",
+        "tick_size": "1.000000"
+    },
+    {
+        "token": "590103",
+        "symbol": "NIFTYBEES",
+        "name": "NIFTYBEES",
+        "expiry": "",
+        "strike": "-1.000000",
+        "lotsize": "1",
+        "instrumenttype": "",
+        "exch_seg": "BSE",
+        "tick_size": "1.000000"
+    }
+]
